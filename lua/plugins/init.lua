3744318874fb13ae7b636f2f559502cb2e98591c
dvp <d.portnov@iterrf.ru>
2024-08-04 17:04:23 +0300

    feat: langmap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,15 @@ return {
     end,
   },
 
+  {
+    'Wansmer/langmapper.nvim',
+    lazy = false,
+    priority = 1, -- High priority is needed if you will use `autoremap()`
+    config = function()
+      require('langmapper').setup() -- try default mapping
+    end,
+  },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
